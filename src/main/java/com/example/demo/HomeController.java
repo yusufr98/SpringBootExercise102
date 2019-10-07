0676ba80a5fe6996cb00786d3230f936c9ea9186
yusufr98 <yusuf090798@gmail.com>
2019-10-07 11:24:57 -0400

    initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("var1","Yusuf");
+        model.addAttribute("var2", "Reyazuddin");
+        return "hometemplate";
+    }
+}
